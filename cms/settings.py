7a53097d62e33a454b16bdbaaf05c36208d14748
thisisbhaven <thisisbhaven@gmail.com>
2021-04-17 02:00:34 +0530

    s3

--- cms/settings.py
+++ cms/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$%*z!jcz6=3tnvmyhm+9z5ls4omf9(9fi8hezff74w9gg=(*wv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['bhavensblog.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -128,15 +129,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_ACCESS_KEY_ID = 'AKIARNWGMFQKOKEK6QG6'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'bhavenscms-bucket'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated', ),
